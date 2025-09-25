917aabd2f54f6f7e6d6e0ce678023b2d77103f3e
Uday Mehtani <udaymehtani@gmail.com>
2025-09-26 02:22:55 +0530

    Create Recursive.cpp

---
+++ 120. Triangle/Recursive.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> v;
+    int n;
+
+    int f(int i, int j, vector<vector<int>> &grid){
+        if(i == 0 && j == 0){
+            return grid[0][0];
+        }
+        int up;
+        if(i == j){
+            up = 1e9;
+        }
+        else up = f(i-1, j, grid);
+        
+        int upleft;
+        if(j == 0){
+            upleft = 1e9;
+        }
+        else {
+           upleft  = f(i-1, j-1, grid);
+    }
+        return min(up, upleft) + grid[i][j];
+    }
+
+    int minimumTotal(vector<vector<int>>& grid) {
+        n = grid.size();
+        // f(i, j) -> minmimum cost to reach point (i, j) from (0, 0)
+        for(int i = 0; i < n; i++){
+            v.push_back(f(n-1, i, grid));
+        }
+        return *min_element(v.begin(), v.end());
+    }
+};
